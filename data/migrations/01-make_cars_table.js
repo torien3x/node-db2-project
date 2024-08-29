a5d32e47c36e1557ad669bb974dce5a0138725e3
Torien3x <tstanley0323@gmail.com>
2024-08-28 22:43:21 -0700

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,19 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 17).notnullable().unique()
+
+    tbl.string('make', 128).notnullable()
+
+    tbl.string('model', 128).notnullable()
+
+    tbl.numeric('mileage').unsigned().notnullable()
+
+    tbl.string('title', 128)
+
+    tbl.string('transmission', 128)
+  })
 };
 
 exports.down = function (knex) {
