1e8a8ece7e08b488f4b9c54383625d50caeb1145
Torien3x <tstanley0323@gmail.com>
2024-08-28 22:24:22 -0700

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 250000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 550000,
+        title: 'salvage',
+    },
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+        title: 'clean',
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//         .truncate().then(() => {
+//             return knex('cars').insert(cars)
+//         })
+// }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
+
